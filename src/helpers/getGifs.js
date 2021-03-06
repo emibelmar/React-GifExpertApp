eba135a63f53f4b2621096bc32fcf7c305beab5f
emiliobeltran94@gmail.com <emiliobeltran94@gmail.com>
2021-03-06 13:08:20 +0100

    First commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,16 @@
+export const getGifs = async (category) => {
+
+    const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&api_key=jJZDeosO31OQJ4Fd9gy7W6l4f8gbBrr1&limit=10`
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+
+    const gifs = data.map(img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+
+    })
+    return gifs;
+}
